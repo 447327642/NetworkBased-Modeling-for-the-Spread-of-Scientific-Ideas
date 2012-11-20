697f6d53f75abd61bb37c7795f8802cceaeae635
rzgar <rzgarh@student.ethz.ch>
2012-11-20 12:23:11 +0100

    Update code/step3b.m

--- code/step3b.m
+++ code/step3b.m
@@ -7,22 +7,20 @@ function intra_idea_distance = step3b(mat,vec)
 unq=unique(vec); %%% collection of distinct ideas
 dd=zeros(1,length(unq)); %%% initializing the vector for storing the average distance between agents holding a particular idea
 n=zeros(1,length(unq)); %%% initializing the corresponding vector for storing the number of agents holding a certain idea
-
+mats=sparse(mat);
 for i=1:length(unq)
     x=find(vec==unq(i));  %%% defining the set of agents holding the idea u(i)
-    n(i)=length(x); %%% number of agents holding the idea i
-    mat1=mat(x,x); %%% focusing on the fraction of the matrices related to agents holding the idea i
-    mat2=sparse(mat1); %%% making mat1 sparse in order to passit to shortest_paths function
+    n(i)=length(x); %%% number of agents holding the idea 
     sum1=0;
     for j=1:n(i)
-        for k=1:n(i)
-            dis=graphshortestpath(mat2,j,k); %%% distance of each agent with similar idea from agent j inside the group [needs the bioinformatics toolbox to be installed]
+        for k=(j+1):n(i)
+            dis=graphshortestpath(mats,x(j),x(k)); %%% distance of each agent with similar idea from agent j inside the group [needs the bioinformatics toolbox to be installed]
             sum1=sum1+dis; %%% sum of the distances
         end
     end
     if n(i)>1
        norml=n(i)*(n(i)-1)/2; %%% The number of distinct pairs inside the group of the agents with the same idea
-       dd(i)=(sum1/2)/norml; %%% average intra_idea distance for idea i
+       dd(i)=sum1/norml; %%% average intra_idea distance for idea i
     else
        dd(i)=0;
     end
