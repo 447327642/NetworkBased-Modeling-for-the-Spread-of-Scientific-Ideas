07b57aed1c5bfc46ac8f3ea7d628dda954f9c925
rzgar <rzgarh@student.ethz.ch>
2012-11-20 12:45:18 +0100

    Update code/step4d.m

--- code/step4d.m
+++ code/step4d.m
@@ -5,17 +5,16 @@ function average_path_length = step4d( mat,s,c )
 %c: the connected component vector
 av_p_l=zeros(1,s); %%% the vector which will store the average path length for each connected component of the 'mat' network
 n=zeros(1,s); %%%  the vector which will store the number of nodes belonging to each connected component.
+sp=sparse(mat);
 
 for i=1:s %%% for each connected component
     x=find(c==i); %%% charachterizing the nodes which belong to the i-th connected component
     a=length(x); %%% the number of nodes in the i-th connected component
     n(i)=a;
-    compnt=mat(x,x); %%% focusing on the nodes belonging to the i-th connected component
-    sp=sparse(compnt);
     count=0;
     for k=1:(a-1)
         for j=(k+1):a
-        count=count+graphshortestpath(sp,k,j); %%% sum of the path length between all pairs of node
+        count=count+graphshortestpath(sp,x(k),x(j)); %%% sum of the path length between all pairs of node
         end
     end
     norml=a*(a-1)/2; %%% normalization factor
