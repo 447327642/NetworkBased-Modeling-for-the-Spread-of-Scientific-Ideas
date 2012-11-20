e58f2a504ad8f6edd8ceb070059009ddba5f3cb4
rzgar <rzgarh@student.ethz.ch>
2012-11-20 11:33:55 +0100

    Update code/step3a.m

--- code/step3a.m
+++ code/step3a.m
@@ -9,8 +9,9 @@ s=zeros(1,length(u)); %%% initializing the vector for storing the number of agen
 for i=1:length(u)
     x=find(vec==u(i));  %%% defining the set of agents holding the idea u(i)
     s(i)=length(x);
+    if s(i)>1
     sum1=0; %%% number of agents with idea i which are in direct neighbourhood
-    for k=1:s(i) %%% These two for loops are used to teat the neighborhoods of all distinct pairs
+    for k=1:s(i) %%% These two for loops are used to test the neighborhoods of all distinct pairs
         for j=(k+1):s(i)
             if mat(x(k),x(j))==1 %%% to check if they are neighbours
                 sum1=sum1+1;
@@ -19,6 +20,9 @@ for i=1:length(u)
     end
     max_neighbors=(s(i)*(s(i)-1)/2); %%% normalizing factor (i.e., the maximum number of pairs of distinct agents)
     w(i)=sum1/max_neighbors; %%% neighborhood index for idea 'i'
+    else
+    w(i)=0;
+    end
 end
 norml=sum(s); %%% Normalizing factor
 count=w.*s; %%% The weighted sum of neighborhood index   
